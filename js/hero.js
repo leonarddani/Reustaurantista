0ca99cf6a42a8041854a8f0a46aab71b3e5c6dca
leonarddani <leonarddani02@gmail.com>
2025-01-09 21:51:04 +0100

    updated javascript for the hero-section

--- js/hero.js
+++ js/hero.js
@@ -56,4 +56,54 @@ function updateTotalPrice(){
     const total = currentFoodPrice * currentQuantity;
     totalPriceElement.textContent = `$ ${total.toFixed(2)}`
 }
-updateTotalPrice()
+
+document.getElementById("increse").addEventListener("click", () =>{
+  updateQuantity(currentQuantity + 1);
+})
+
+document.getElementById("degress").addEventListener("click", () =>{
+  
+})
+
+
+function selectFoodItem(selectedFood, selectedElement){
+  updateHeroImage(selectedFood);
+  currentFoodPrice = selectedFood.price;
+  updateQuantity(currentQuantity);
+
+
+  const allFodItems = document.querySelectorAll(".food-item");
+  allFodItems.forEach((item) => item.classList.remove(selected))
+  selectedElement.classList.add("selected")
+}
+
+
+function updateHeroImage(food){
+  const heroImage = document.querySelector(".hero-main-image");
+  const foodTitle = document.querySelector(".food-title p:first-of-type");
+  const foodRating = document.querySelector(".food-title p:first-of-type");
+  const perparationTime = document.querySelector(".perpare-time");
+  heroImage.src = food.image;
+  heroImage.alt = food.name;
+  foodTitle.textContent = food.name;
+  foodRating.innerHTML = `<i class="fa-solid fa-star"</i>${food.rate}`
+  perparationTime.innerHTML = `<i class="fa-solid fa-clock"</i>${food.perparationTime}`
+}
+
+function updateVIsibleItems(foods){
+  const foodItems = document.querySelectorAll(".food-item");
+ foodItems.forEach((item, index) => {
+  if(index >= currentIndex && index < currentIndex + visibleItemsCount)
+  }else{
+    item.style.display = "none";
+ });
+  if(index >= currentIndex && index < currentIndex + visibleItemsCount)
+}else{
+  item.style.display = "none";
+}
+
+
+function setupArrowNavigation(foods){
+  const lefArrow = document.querySelector('.left-arrow')
+  const rightArrow = document.querySelector('.right-arrow')
+}
