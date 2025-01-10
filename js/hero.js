55a78fb80390d8806968462be12c3c6354a8b5a2
leonarddani <leonarddani02@gmail.com>
2025-01-10 16:25:28 +0100

    added ads for testing

--- js/hero.js
+++ js/hero.js
@@ -6,79 +6,86 @@ async function fetchFoodData() {
   try {
     const response = await fetch("js/food.json");
     const foodData = await response.json();
-    const intialCategory = "Lunch"
-    renderFoodCarusel(foodData.categories[intialCategory])
+    const intialCategory = "Lunch";
+    renderFoodCarusel(foodData.categories[intialCategory]);
 
-    updateHeroImage(foodData.categories[intialCategory][0])
+    updateHeroImage(foodData.categories[intialCategory][0]);
 
-    setupArrowNavigation(foodData.categories[intialCategory])
+    setupArrowNavigation(foodData.categories[intialCategory]);
   } catch (error) {
     console.log("error fetching food data");
-    
   }
 }
 
-function renderFoodCarusel(foods){
-    const foodItemsContainer = document.querySelector(".food-items");
-    foodItemsContainer.innerHTML = "";
+function renderFoodCarusel(foods) {
+  const foodItemsContainer = document.querySelector(".food-items");
+  foodItemsContainer.innerHTML = "";
 
-    foods.foreach((food, index) => {
-        const foodItem = document.createElement("div");
-        foodItem.classList.add("food-item");
-        if(index === 0){
-            foodItem.classList.add("selected");
-        }
+  foods.foreach((food, index) => {
+    const foodItem = document.createElement("div");
+    foodItem.classList.add("food-item");
+    if (index === 0) {
+      foodItem.classList.add("selected");
+    }
 
-        foodItem.innerHTML = `
+    foodItem.innerHTML = `
         <img src="${food.image}" alt="${food.name}">
 
         <p>
         ${food.name} <br>
-        <span class="item-price></span> <span class="valute>$</span> <span>${food.price.toFixed(2)}</span>
+        <span class="item-price></span> <span class="valute>$</span> <span>${food.price.toFixed(
+          2
+        )}</span>
         </P>
-        `
+        `;
 
-        if(index >= visibleItemsCount){
-            foodItem.style.display = "none";
-        }
+    if (index >= visibleItemsCount) {
+      foodItem.style.display = "none";
+    }
 
-        foodItem.addEventListener("click", () => {
-
-            selectFoodItem(food, foodItem);
-            currentIndex = index
-        })
-    })
-    
+    foodItem.addEventListener("click", () => {
+      selectFoodItem(food, foodItem);
+      currentIndex = index;
+    });
+  });
 }
 
-function updateTotalPrice(){
-    const totalPriceElement = querySelector(".order-info-total .price");
-    const total = currentFoodPrice * currentQuantity;
-    totalPriceElement.textContent = `$ ${total.toFixed(2)}`
+function updateTotalPrice() {
+  const totalPriceElement = querySelector(".order-info-total .price");
+  const total = currentFoodPrice * currentQuantity;
+  totalPriceElement.textContent = `$ ${total.toFixed(2)}`;
 }
 
-document.getElementById("increse").addEventListener("click", () =>{
+document.getElementById("increase").addEventListener("click", () => {
   updateQuantity(currentQuantity + 1);
-})
+});
 
-document.getElementById("degress").addEventListener("click", () =>{
-  
-})
+document.getElementById("decrease").addEventListener("click", () => {
+  if (currentQuantity > 1) {
+    updateQuantity(currentQuantity - 1);
+  }
+});
+
+function updateQuantity(newQuantity) {
+  currentQuantity = newQuantity;
+  document.querySelector(".quantity").textContent = currentQuantity;
+  updateTotalPrice();
+}
 
 
-function selectFoodItem(selectedFood, selectedElement){
+function selectFoodItem(selectedFood, selectedElement) {
   updateHeroImage(selectedFood);
+
   currentFoodPrice = selectedFood.price;
+  currentQuantity = 1;
   updateQuantity(currentQuantity);
 
-
   const allFodItems = document.querySelectorAll(".food-item");
-  allFodItems.forEach((item) => item.classList.remove(selected))
-  selectedElement.classList.add("selected")
+  allFodItems.forEach((item) => item.classList.remove(selected));
+  selectedElement.classList.add("selected");
 }
 
-
-function updateHeroImage(food){
+function updateHeroImage(food) {
   const heroImage = document.querySelector(".hero-main-image");
   const foodTitle = document.querySelector(".food-title p:first-of-type");
   const foodRating = document.querySelector(".food-title p:first-of-type");
@@ -86,24 +93,79 @@ function updateHeroImage(food){
   heroImage.src = food.image;
   heroImage.alt = food.name;
   foodTitle.textContent = food.name;
-  foodRating.innerHTML = `<i class="fa-solid fa-star"</i>${food.rate}`
-  perparationTime.innerHTML = `<i class="fa-solid fa-clock"</i>${food.perparationTime}`
+  foodRating.innerHTML = `<i class="fa-solid fa-star"</i>${food.rate}`;
+  perparationTime.innerHTML = `<i class="fa-solid fa-clock"</i>${food.perparationTime}`;
 }
 
-function updateVIsibleItems(foods){
+function updateVIsibleItems(foods) {
   const foodItems = document.querySelectorAll(".food-item");
- foodItems.forEach((item, index) => {
-  if(index >= currentIndex && index < currentIndex + visibleItemsCount)
-  }else{
-    item.style.display = "none";
- });
-  if(index >= currentIndex && index < currentIndex + visibleItemsCount)
-}else{
-  item.style.display = "none";
+
+  foodItems.forEach((item, index) => {
+    if (index <= currentIndex && index < currentIndex + visibleItemsCount) {
+      item.style.display = "none";
+    } else {
+      item.style.display = "block";
+    }
+  });
 }
 
+function setupArrowNavigation(foods) {
+  const lefArrow = document.querySelector(".left-arrow");
+  const rightArrow = document.querySelector(".right-arrow");
 
-function setupArrowNavigation(foods){
-  const lefArrow = document.querySelector('.left-arrow')
-  const rightArrow = document.querySelector('.right-arrow')
+  leftArrow.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateVisibleItems(foods);
+    }
+  });
+
+  rightArrow.addEventListener("click", () => {
+    if (currentIndex < foods.length - visibleItemsCount) {
+      currentIndex++;
+      updateVisibleItems(foods);
+    } else {
+      currentIndex = 0;
+    }
+    updateVisibleItems(foods);
+  });
 }
+
+function addToCard(selectedFood) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const exestingItemIndex = cart.findIndex(
+    (item) => item.name === selectedFood.name
+  );
+  if (exestingItemIndex !== -1) {
+    cart[exestingItemIndex].quantity += currentQuantity;
+  } else {
+    cart.push({
+      name: selectedFood.name,
+      price: seleectedFood.price,
+      image: selectedFood.image,
+      quantity: currentQuantity,
+    });
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCardBadge();
+}
+
+function updateCardBadge() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let totalUniqueItems = addToCard.length;
+  document.getElementById("cart-badge").textContent = totalUniqueItems;
+}
+
+document.querySelector(".add-to-cart").addEventListener("click", () => {
+  const selectedFood = {
+    name: document.querySelector(".food-title p:first-of-type").textContent,
+    price: currentFoodPrice,
+    image: document.querySelector(".hero-main-image").src,
+  };
+  addToCard(selectedFood);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchFoodData();
+  updateCardBadge();
+});
